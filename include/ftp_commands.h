7d83723d754e0e883692ae04f4cfefc81b99df32
Deividas <154059572+YBDYY@users.noreply.github.com>
2025-01-15 18:05:32 +0200

    updated ftp_commands.h and ftp_utils.h, added comments for docs

--- include/ftp_commands.h
+++ include/ftp_commands.h
@@ -1,8 +1,29 @@
 #ifndef FTP_COMMANDS_H
 #define FTP_COMMANDS_H
 
-void send_command(int sock, const char *command);
-void list_files(int sock);
-void download_file(int sock, const char *filename);
+#include "ftp_client.h"  // Include the FTP client structure for function prototypes
 
-#endif 
+/**
+ * Send the USER command to the FTP server.
+ * @param client: Pointer to the FTPClient instance.
+ * @param username: The username to authenticate with.
+ * @return true if the command was sent successfully, false otherwise.
+ */
+bool send_user_command(FTPClient *client, const char *username);
+
+/**
+ * Send the PASS command to the FTP server.
+ * @param client: Pointer to the FTPClient instance.
+ * @param password: The password to authenticate with.
+ * @return true if the command was sent successfully, false otherwise.
+ */
+bool send_pass_command(FTPClient *client, const char *password);
+
+/**
+ * Send the QUIT command to the FTP server.
+ * @param client: Pointer to the FTPClient instance.
+ * @return true if the command was sent successfully, false otherwise.
+ */
+bool send_quit_command(FTPClient *client);
+
+#endif // FTP_COMMANDS_H
--- include/ftp_utils.h
+++ include/ftp_utils.h
@@ -1,7 +1,20 @@
 #ifndef FTP_UTILS_H
 #define FTP_UTILS_H
 
-void handle_error(const char *message);
-char *parse_response(const char *response);
+#include "ftp_client.h"  // Include the FTP client structure for function prototypes
 
-#endif 
+/**
+ * Parse an FTP server's response and return a status code.
+ * @param response: The server's response string.
+ * @return Integer status code based on response.
+ */
+int parse_ftp_response(const char *response);
+
+/**
+ * Validate an FTP server response.
+ * @param response: The server's response string.
+ * @return true if the response is valid, false otherwise.
+ */
+bool is_valid_ftp_response(const char *response);
+
+#endif // FTP_UTILS_H
